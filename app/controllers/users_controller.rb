61a177ffd52603e9d9fb52911c214237206cdbc2
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2021-05-28 12:29:19 -0500

    Add field is admin to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:full_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:full_name, :email, :is_admin, :password, :password_confirmation)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_024935) do
+ActiveRecord::Schema.define(version: 2021_05_28_172533) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_05_28_024935) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
