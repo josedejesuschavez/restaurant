38bc2ec6f260c8c4d82454faa99a1bbd7ed08437
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2021-05-28 16:16:59 -0500

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(full_name: "Jose Chavez", email: "jchavez@arkusnexus.com", password_digest: "123qweQWE", is_admin: true)
